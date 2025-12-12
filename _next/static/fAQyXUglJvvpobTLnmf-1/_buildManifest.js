95810ed64708d0682d959c78a548e4f6ea35cf85
devrus91 <ruslan.fedoseenko@protonmail.com>
2025-12-13 02:59:52 +0300

    Updates

--- _next/static/fAQyXUglJvvpobTLnmf-1/_buildManifest.js
+++ _next/static/fAQyXUglJvvpobTLnmf-1/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/dragy-analyzer//_next/:path+",
+        "destination": "/dragy-analyzer/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
